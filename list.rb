4a74593e2609545c667e36be5d8154bf8dc3bbc8
Juraci Paixão Kröhling <juraci@kroehling.de>
2018-11-05 12:16:54 +0100

    Sleep at some points, to not reach the max # of requests per minute
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- list.rb
+++ list.rb
@@ -14,6 +14,8 @@ baseUrl = "https://api.github.com"
 month_range = (Time.gm(year, month, 1)..Time.gm(year, month.to_i+1, 1))
 month_contributors = {}
 
+num_requests = 0
+
 if token.nil? then
     STDERR.puts "GitHub credentials missing. Export FTC_GITHUB_USERNAME and FTC_GITHUB_TOKEN."
     exit 1
@@ -21,10 +23,14 @@ end
 
 ## get all the repositories under the organization
 RestClient::Request.execute method: :get, url: "#{baseUrl}/orgs/#{organization}/repos?per_page=100", user: username, password: token do | resp1 | 
+    sleep(2) # make it easy on github, they allow only up to 30 requests per minute, or one every two seconds
+    num_requests += 1
     repos = JSON.parse(resp1.body)
     repos.each do | repo | 
         ## get the first 100 most-recently closed PRs from this repository
         RestClient::Request.execute method: :get, url: "#{baseUrl}/repos/#{organization}/#{repo['name']}/pulls?state=closed&sort=created&direction=desc&per_page=100", user: username, password: token do | resp2 | 
+            sleep(2)
+            num_requests += 1
             prs = JSON.parse(resp2.body)
             oldest_merge = Time.now
             prs.each do | pr | 
@@ -58,12 +64,22 @@ month_contributors.sort.uniq.each do | k, v |
     q = "is:pr author:#{k} org:#{organization} is:merged"
     ## get the oldest merged PR from this user across all repositories in the organization
     RestClient::Request.execute method: :get, url: "#{baseUrl}/search/issues?sort=created&order=asc&per_page=1&q=#{URI.escape(q)}", user: username, password: token do | resp | 
-        pr = JSON.parse(resp.body)['items'].first
+        sleep(2)
+        num_requests += 1
+        prs = JSON.parse(resp.body)['items']
+        if prs.nil? then
+            puts "Expected to get some PRs for user #{k} but got none."
+            STDERR.puts resp.body
+            next
+        end
+        pr = prs.first
         closed_at = Time.parse(pr['closed_at'])
         ## if the oldest PR was merged this month, then we have a first time contributor
         if month_range.cover?(closed_at) then
-            puts "#{closed_at};#{k};#{pr['html_url']}"
+            puts "#{closed_at};#{k};https://github.com/#{k};#{pr['html_url']}"
         end
     end
 end
 
+puts
+puts "Number of requests in total: #{num_requests}"
